15a14410eb1755615f73d7883e6a1a0d83037bf9
Rineta2 <rzkir571@gmail.com>
2025-05-14 07:35:11 +0700

    [update] testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import React, { Fragment } from 'react'
 
-import ProductsList from '@/components/ui/products/Products'
+// import ProductsList from '@/components/ui/products/Products'
 
 import HomeLayout from '@/components/ui/home/HomeLayout'
 
-import ServiceList from '@/components/ui/service/Service'
+// import ServiceList from '@/components/ui/service/Service'
 
-import About from "@/components/ui/about/About"
+// import About from "@/components/ui/about/About"
 
 export default function page() {
   return (
