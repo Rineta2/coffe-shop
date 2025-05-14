d335095a294c2b77c40cdaf8304cb0002410a3c6
Rineta2 <rzkir571@gmail.com>
2025-05-14 07:43:12 +0700

    [update] bug to production

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,20 @@
 import React, { Fragment } from 'react'
 
-// import ProductsList from '@/components/ui/products/Products'
+import ProductsList from '@/components/ui/products/Products'
 
 import HomeLayout from '@/components/ui/home/HomeLayout'
 
-// import ServiceList from '@/components/ui/service/Service'
+import ServiceList from '@/components/ui/service/Service'
 
-// import About from "@/components/ui/about/About"
+import About from "@/components/ui/about/About"
 
 export default function page() {
   return (
     <Fragment>
       <HomeLayout />
-      {/* <ServiceList /> */}
-      {/* <About /> */}
-      {/* <ProductsList /> */}
+      <ServiceList />
+      <About />
+      <ProductsList />
     </Fragment>
   )
 }
